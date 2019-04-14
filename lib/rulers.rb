e7f3e6393cc72090ba9842b622ee1baaa7ffbc37
SOO YANG, TAN <sooyang.code@gmail.com>
2019-04-14 22:17:35 +0800

    Chapter 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,31 @@
 require "rulers/version"
 require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+      if env['PATH_INFO'] == '/'
+        return [301, {'Content-Type' => 'text/html', 'Location' => 'http://localhost:3001/quotes/a_quote'}, []]
+      end
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+      [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    def env
+      @env
     end
   end
 end
